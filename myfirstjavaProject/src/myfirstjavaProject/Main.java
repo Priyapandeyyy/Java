9982d28e87a43da0f78a5db9078f80fd174594ea
Priya Pandey <priya.pandeyy321@gmail.com>
2024-03-08 23:05:41 +0530

    Initial commit

---
+++ myfirstjavaProject/src/myfirstjavaProject/Main.java
@@ -0,0 +1,31 @@
+package myfirstjavaProject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\Hello World!!\"");
+		System.out.println("I did It!!");
+		System.out.println();
+		
+	}
+}
+
+// Class = It is a collection of related code.
+
+// This is in source code and we are going to convert this source code into 
+
+ /*Public = it is an access specifier, means it will be accessed by publically.
+ Static = It is access modifier, that means when the java program is load then it will create the space in memory automatically.
+ Void = It is a return type that is it does not return any value. main(); (it is a method or a function name).
+ */
+
+// We can either use println or we can use \n (both will do the same work).
+
+// If we use both then we are going to have an additional empty line.
+
+
+/* \t = add extra tab
+ "\" = add double quote in output
+ \\ = to show backslash in output.
+*/
