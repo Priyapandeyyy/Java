e153f0c1b8a96ac4c3d8f74f438413ad657ad66b
Priya Pandey <priya.pandeyy321@gmail.com>
2024-03-10 00:38:57 +0530

    added new file Expression and GUI

---
+++ myfirstjavaProject/src/myfirstjavaProject/Expression.java
@@ -0,0 +1,24 @@
+// Expression = It is a construct made up of variables, operators, and method invocations, which are constructed according to the syntax of the language, that evaluates to a single value.
+
+
+package myfirstjavaProject;
+
+public class Expression {
+	
+	public static void main(String[] args) {
+		
+		// expression = operands & operators.
+		// operands = values, variables, numbers, quantity.
+		// operators = + - * / %
+		
+		double Novels = 20;
+		
+		Novels = (double) Novels / 3;  // It will give  6 instead of 6.666 because in integer devision we truncate any decimal portion.
+		
+		// We can cast a value as ()
+		
+		// Novels--;   // This or Novels = Novels +1;
+		
+		System.out.println(Novels);
+	}
+}
